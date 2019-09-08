9b14e869ece4770227c92275de6017e298c0c179
harikrishnans93 <harikrishnans.93@gmail.com>
2019-09-08 17:26:55 +0530

    index

--- ngTrainingApp/src/app/userdetails/details.component.ts
+++ ngTrainingApp/src/app/userdetails/details.component.ts
@@ -15,7 +15,7 @@ export class DetailsComponent implements OnInit {
 
   onToggleDetails() {
     this.Status = !this.Status;
-    this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 
 }
