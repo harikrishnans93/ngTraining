02b30d858d054268b091d25b4b6bfbdbb663b915
harikrishnans93 <harikrishnans.93@gmail.com>
2019-09-09 22:57:49 +0530

    fixing navigation

--- courseProject/src/app/app.component.ts
+++ courseProject/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'courseProject';
+  constructor() {
+
+  }
+  feature = '';
+  navigate( menuitem: string ) {
+    this.feature = menuitem;
+  }
+  ngOnInit() {
+
+  }
 }
