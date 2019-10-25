ef3983f42953c7b61a3c8e1a0c392042711a805c
harikrishnans93 <harikrishnans.93@gmail.com>
2019-10-25 15:53:41 +0530

    test

--- courseProject/src/app/recipe/recipe-list/recipe-list.component.ts
+++ courseProject/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -3,6 +3,7 @@ import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -12,25 +13,35 @@ import { Subscription } from 'rxjs';
 export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
   recipesub = new Subscription();
-  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {
+  // tslint:disable-next-line: max-line-length
+  constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute, private datasService: DataStorageService) {
+
     this.recipesub = this.recipeService.recipiesAdded.subscribe((recipies: Recipe[]) => {
-      console.log(this.recipes, 'REcipesssscon'); 
+      console.log(this.recipes, 'REcipesssscon');
       this.recipes = recipies;
     });
+    datasService.loadRecipeData();
   }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipies();
     console.log('init');
     this.recipesub = this.recipeService.recipiesAdded.subscribe((recipies: Recipe[]) => {
-        console.log(this.recipes, 'REcipessss');
-        this.recipes = recipies;
-      });
+      console.log(this.recipes, 'REcipessss');
+      this.recipes = recipies;
+    });
+    this.datasService.loadRecipeData();
   }
   ngOnDestroy() {
     this.recipesub.unsubscribe();
   }
   addRecipe() {
     this.router.navigate(['new'], { relativeTo: this.route });
+  }
+  LoadRecipes() {
+    this.recipesub = this.recipeService.recipiesAdded.subscribe((recipies: Recipe[]) => {
+      this.recipes = recipies;
+    });
+    this.datasService.loadRecipeData();
   }
 }
